455e86d13ca968ae3e0ce96dd50610e5fa768f51
zenin5479 <zenin5479@gmail.com>
2026-02-17 15:52:47 +0300

    Update 7.5 Обновлен метод. Проведен рефакторинг

--- PassLinkControlMethod.cs
+++ PassLinkControlMethod.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PassLinkControlToClass
 {
@@ -20,7 +22,26 @@ namespace PassLinkControlToClass
       public void DoWork()
       {
          if (_textBox != null)
+         {
             _textBox.Text = "Работа выполнена";
+         }
+
+         if (_listBox != null)
+         {
+            // ListBox
+            //_listBox.Items.Add(item);
+            // Прокрутка ListBox вниз
+            _listBox.TopIndex = _listBox.Items.Count - 1;
+         }
+
+         if (_richTextBox != null)
+         {
+            // RichTextBox
+            //_richTextBox.AppendText(text);
+            _richTextBox.AppendText(Environment.NewLine);
+            // Прокрутка RichTextBox вниз
+            _richTextBox.ScrollToCaret();
+         }
       }
    }
 }
