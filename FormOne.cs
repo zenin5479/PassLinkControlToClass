53da0c1b0a414fa68f442c8fa66fcc684554d0ce
zenin5479 <zenin5479@gmail.com>
2026-02-17 10:33:35 +0300

    Update 3.0 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -4,14 +4,22 @@ namespace PassLinkControlToClass
 {
    public partial class FormOne : Form
    {
+      private PassLinkControlConstructor _manager;
+
       public FormOne()
       {
          InitializeComponent();
+
       }
 
       private void ButtonConstructor_Click(object sender, System.EventArgs e)
       {
-
+         // Создаём менеджер и передаём ссылки на элементы
+         _manager = new PassLinkControlConstructor(
+            textBox1,   // ваш TextBox на форме
+            listBox1,    // ваш ListBox на форме
+            richTextBox1 // ваш RichTextBox на форме
+         );
       }
 
       private void ButtonMethod_Click(object sender, System.EventArgs e)
