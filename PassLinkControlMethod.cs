cc7b3057a5bff7a3ca143d1bf8b6e0209f6508c4
zenin5479 <zenin5479@gmail.com>
2026-02-17 15:43:27 +0300

    Update 6.9 Обновлен метод. Проведен рефакторинг

--- PassLinkControlMethod.cs
+++ PassLinkControlMethod.cs
@@ -1,8 +1,26 @@
-﻿namespace PassLinkControlToClass
+﻿using System.Windows.Forms;
+
+namespace PassLinkControlToClass
 {
    // Передача ссылок на элементы управления через метод
    public class PassLinkControlMethod
    {
+      private TextBox _textBox;
+      private ListBox _listBox;
+      private RichTextBox _richTextBox;
 
+      // Метод для передачи элементов
+      public void SetControls(TextBox textBox, ListBox listBox, RichTextBox richTextBox)
+      {
+         _textBox = textBox;
+         _listBox = listBox;
+         _richTextBox = richTextBox;
+      }
+
+      public void DoWork()
+      {
+         if (_textBox != null)
+            _textBox.Text = "Работа выполнена";
+      }
    }
 }
