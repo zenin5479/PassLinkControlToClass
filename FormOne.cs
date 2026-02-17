d85e259ee7e18248c1e859396e4a1300914dc19b
zenin5479 <zenin5479@gmail.com>
2026-02-17 10:43:33 +0300

    Update 4.0 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -15,9 +15,9 @@ namespace PassLinkControlToClass
       {
          // Создаём менеджер и передаём ссылки на элементы// ваш TextBox на форме // ваш ListBox на форме// ваш RichTextBox на форме
          _manager = new PassLinkControlConstructor(TextBoxReader, ListBoxReader, RichTextBoxReader);
-         _manager.SetTextBoxText("Привет из менеджера!");
-         _manager.AddToListBox("Элемент 1");
-         _manager.AppendToRichTextBox("Строка в RichTextBox");
+         _manager.SetTextBoxText("Передача ссылок через конструктор");
+         _manager.AddToListBox("Передача ссылок через конструктор");
+         _manager.AppendToRichTextBox("Передача ссылок через конструктор");
       }
 
       // Пример: кнопка, которая использует менеджер
