b3ba26a52f1c4056e25af524b2ac1b80207ce3c6
zenin5479 <zenin5479@gmail.com>
2026-02-18 08:55:31 +0300

    Update 9.9 Обновлен метод. Проведен рефакторинг

--- PassLinkControlMethod.cs
+++ PassLinkControlMethod.cs
@@ -31,14 +31,7 @@ namespace PassLinkControlToClass
             _textBox.ScrollToCaret();
          }
 
-         if (_listBox != null)
-         {
-            // ListBox
-            string text = "Передача ссылки на ListBox через метод";
-            _listBox.Items.Add(text);
-            // Прокрутка ListBox вниз
-            _listBox.TopIndex = _listBox.Items.Count - 1;
-         }
+        
 
          if (_richTextBox != null)
          {
