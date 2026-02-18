df798ba30de478b30d8df1f41b59b33eeca30921
zenin5479 <zenin5479@gmail.com>
2026-02-18 09:03:30 +0300

    Update 10.8 Обновлен метод. Проведен рефакторинг

--- PassLinkControlMethod.cs
+++ PassLinkControlMethod.cs
@@ -24,7 +24,6 @@ namespace PassLinkControlToClass
          {
             // TextBox
             // Добавляет строку оставляя предыдущие
-            //string text = "Передача ссылки на TextBox через метод";
             _textBox.AppendText(text);
             _textBox.AppendText(Environment.NewLine);
             // Прокрутка TextBox вниз
